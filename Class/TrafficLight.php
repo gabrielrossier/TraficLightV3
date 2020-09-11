3c9e527da65a615db97898d41da53685b08eeb41
Rossier Gabriel <Rossier.GABRIEL@cpnv.ch>
2020-09-11 09:53:36 +0200

    Have a working solutions

--- Class/TrafficLight.php
+++ Class/TrafficLight.php
@@ -16,57 +16,72 @@ class TrafficLight
                 $this->red = true;
                 $this->yellow = false;
                 $this->green = false;
+                $this->ActualState = 0;
                 break;
             case 1:
                 $this->red = true;
                 $this->yellow = true;
                 $this->green = false;
+                $this->ActualState = 1;
                 break;
             case 2:
                 $this->red = false;
                 $this->yellow = false;
                 $this->green = true;
+                $this->ActualState = 2;
                 break;
             case 3:
                 $this->red = false;
                 $this->yellow = true;
                 $this->green = false;
+                $this->ActualState = 3;
                 break;
             case 4:
                 $this->red = false;
                 $this->yellow = true;
                 $this->green = false;
+                $this->ActualState = 4;
+                break;
+            case 5:
+                $this->red = false;
+                $this->yellow = true;
+                $this->green = false;
+                $this->ActualState = 5;
                 break;
             default:
                 $this->red = true;
                 $this->yellow = false;
                 $this->green = false;
+                $this->ActualState = 0;
         }
+        $_SESSION['myLight'] = $this;
     }
+
+
 
     public function nextState($requiredState)
     {
-        if ($requiredState < 3 and $this->ActualState != 4)
-        {
+        if ($requiredState <= 4) {
             $this->setState(($requiredState) % 4);
+        } elseif ($requiredState == 5 && ($this->ActualState == 0 || $this->ActualState == 2)) {
 
+            $this->setState(5);
+        } elseif ($this->ActualState == 5 && $requiredState >= 0) {
 
+            $this->setState(0);
         }
-        elseif ($requiredState == 4 and ($this->ActualState == 0 or 2))
-        {
-            $this->setState($requiredState);
-        }
-        elseif ($this->ActualState == 4 and $requiredState == 0)
-        {
-            $this->setState($requiredState);
-        }
-
     }
+
+
 
     public function isNextRequired()
     {
-        //Checking if button was pressed then redirect to prevent from re-submit form
 
+        $this->nextState($this->ActualState + 1 % 4);
     }
 
+    public function requireHS()
+    {
+        $this->nextState(5);
+    }
 }
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 require_once("Class/TrafficLight.php");
 session_start();
-
 $Light = isset($_SESSION['myLight'])? $_SESSION['myLight'] : new TrafficLight();
 if (isset($_GET['next'])) {
-    $Light->nextState($Light->ActualState + 1 );
+    
+    $Light->isNextRequired();
 }
 elseif (isset($_GET['HS']))
 {
-    $Light->nextState(4);
+    $Light->requireHS();
 }
 
 
@@ -32,7 +32,7 @@ elseif (isset($_GET['HS']))
     <div class="<?= $Light->yellow ? "CircleYellow" : "CircleOff" ?>"></div>
     <div class="<?= $Light->green ? "CircleGreen" : "CircleOff" ?>"></div>
 
-</div>
+</div class="buttons">
 <a class="btn" href="index.php?next=">&#xbb;</a>
-
+<a class="btn" href="index.php?HS="> HS</a>
 </body>
